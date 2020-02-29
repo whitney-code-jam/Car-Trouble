074c79efd1c5c9660b67db2ab8d1d236cfdd2667
tjohan053 <tjohan053@gmail.com>
2020-02-29 11:27:18 -0800

    Got Car Moving!

---
+++ Car Trouble/Assets/Scripts/carMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMovement : MonoBehaviour
+{
+    [SerializeField]private float speedVel;
+    [SerializeField]private float turningVel;
+    [SerializeField]private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        speedVel = Input.GetAxisRaw("Vertical");
+        turningVel = Input.GetAxisRaw("Horizontal");
+
+        rb.AddForce(transform.forward * speedVel);
+    }
+}
